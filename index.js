2a30c6e81f65cbca4f70a45f5a0b7257c306274b
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-18 12:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,14 +4,7 @@ function happyHolidays () {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(name), (){
-  if name = "you"
+function happyHolidaysTo(name) = ("Mike"){
+  if "(name" = "Mike")
   return "Happy holiday, ${name}!"
 }
-
-
- describe('happyHolidaysTo(name)', () => {
-    it('returns "Happy holidays, ${name}!"', () => {
-      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
-    })
-  })
