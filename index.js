c1d2c3cfdf8df72d509f5ac510d1c823c113ffef
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-18 13:11:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
+}
+function holidayCountdown(holiday, days) {
+  return "it\'s ${days} days until ${holiday}!";
 }
