53be0b2e3161229b07cda472725277b23fcf99b7
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-18 13:04:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidayTo(holiday, name){
-  return "Happy holidays ${holiday}, ${name}!";
+function happyHolidaysTo(holiday, name){
+  return "Happy ${holiday}, ${name}!";
 }
