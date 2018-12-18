6dde52b2d1c8f0f3abea16b25c71163085de4cb1
Mihalis Caramihai <mihalis@caramihai.com>
2018-12-18 13:13:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ function happyHolidayTo(holiday, name){
 function holidayCountdown(holiday, days) {
   return "It\'s ${days} days until ${holiday}!";
 }
+ 1) functions holidayCountdown(holiday, days) returns "It's ${days} days until${holiday}!":
+
+      Error: Expected 'It\'s ${days} days until ${holiday}!' to equal 'It\'s 20days until Mother\'s Day!'
+      + expected - actual
+
+      -It's ${days} days until ${holiday}!
+      +It's 20 days until Mother's Day!
+
+      at assert (node_modules/expect/lib/assert.js:29:9)
+      at Expectation.toEqual (node_modules/expect/lib/Expectation.js:81:30)
+      at Context.it (test/index-test.js:24:52)
